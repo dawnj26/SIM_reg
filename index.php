96d6abf9d8420dbfc8931cb9ccb68cbe93892155
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2023-10-01 12:09:54 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIM Registration</title>
+    <link rel="stylesheet" href="styles/sim_registration.css">
+</head>
+
+<body>
+    <div class="main-container">
+        <div class="header">
+            <div class="description">
+                <h1 class="title">
+                    Register your SIM now!
+                </h1>
+                <p>
+                    In Compliance with the Republic Act No. 11934, all active SIM cards will need to be registered to
+                    keep
+                    the scammers at bay. <span>Register your SIM with use now!</span>
+                </p>
+                <div class="logos">
+                    <img src="./images/PSU-Intel.png" alt="">
+                    <img src="./images/smart-globe.png" alt="">
+                </div>
+            </div>
+
+            <img src="./images/phone-alert.png" alt="" class="phone-alert">
+        </div>
+        <div class="footer">
+            <p>By completing this form, I understand and agree that any personal data I will provide for SIM
+                Registration will be processed in accordance with SIM Registration Act, the Data Privacy Act of 2012,
+                and the Privacy Policy of Smart Globe.</p>
+                <form action="" method="get">
+                    <input type="tel" name="mobile" id="mobile">
+                    <input type="submit" value="Register">
+                </form>
+        </div>
+    </div>
+</body>
+
+</html>
